ca83e9e6e68a17fd51fa46db7889899a879b0885
Matt Hinchliffe <matt.hinchliffe@ft.com>
2018-05-09 14:55:10 +0100

    Add teaser decisions tree docs
     🐿 v2.8.0

---
+++ secret-squirrel.js
@@ -0,0 +1,13 @@
+module.exports = {
+	files: {
+		allow: [
+			'docs/Teaser_Prefix_2018-05-03.pdf',
+			'docs/Teaser_Tag_2018-05-03.pdf'
+		],
+		allowOverrides: []
+	},
+	strings: {
+		deny: [],
+		denyOverrides: []
+	}
+};
